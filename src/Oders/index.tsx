22c8600032e0e40b2da1ac64c36f81e6bf53805d
Ernando <ernando.rezende@dtidigital.com.br>
2021-01-11 00:14:30 -0300

    Search Order Locatio

--- src/Oders/index.tsx
+++ src/Oders/index.tsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
+import ProductsList from './ProductsList';
 import StepsHeader from './StepsHeader';
 import './style.css';
+import { OrderLocationData, Product } from './types';
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation, setOrderLoation] = useState<OrderLocationData>();
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.log(error));
+            
+    }, []);
+
     return (
         <div className="orders-container">
             <StepsHeader/>
+            <ProductsList products={products} />
+            <OrderLocation onChangeLocation={location => setOrderLoation(location)} />
         </div>
     )
 }
